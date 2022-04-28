239e6d0c3bf18b254931126f982a8c11c39ffccd
NK Studio <53283888+bnm000215@users.noreply.github.com>
2022-04-29 03:56:23 +0900

    re

---
+++ Editor/About/SnapToFloorSettings.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NKStudio.UnitySnapToFloor.Editor
+{
+    public class SnapToFloorSettings : ScriptableObject
+    {
+        public static readonly Dictionary<KLanguage, List<string>> StartAtShowText = new()
+        {
+            {KLanguage.English, new List<string>() {"Always", "Never",}},
+            {KLanguage.한국어, new List<string>() {"항상", "끄기",}}
+        };
+
+        public enum KSnapMode
+        {
+            Mode3D,
+            Mode2D,
+        }
+
+        public enum KLanguage
+        {
+            English,
+            한국어,
+        }
+
+        public enum KStartAtShow
+        {
+            Always,
+            Never,
+        }
+
+        [field: SerializeField] public KSnapMode Mode { get; set; }
+
+        [field: SerializeField] public KLanguage Language { get; set; }
+
+        [field: SerializeField] public KStartAtShow StartAtShow { get; set; }
+    }
+}
